ecd6e8f6353320fe52b699f6a102305a4ff0d20c
RogwiCajas <rogwinalex2@hotmail.com>
2020-07-03 21:47:53 -0500

    instalacion de dependecias JavaFX

---
+++ SistemadeTurnos/src/sistemadeturnos/SistemaDeTurnos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadeturnos;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author cajas
+ */
+public class SistemaDeTurnos extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
